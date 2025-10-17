20493feebe69d01ba2b820775c69fbdb5d93870f
gummipunkt <patrick@gummipunkt.eu>
2025-10-17 11:38:14 +0200

    Added screenshots to README Added user management to admin panel Added SMTP for password reset emails Updated README

--- server/app/controllers/admin/users_controller.rb
+++ server/app/controllers/admin/users_controller.rb
@@ -11,6 +11,19 @@ class Admin::UsersController < Admin::BaseController
     @accounts = @user.provider_accounts.order(:provider, :handle)
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: "User updated"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     user = User.find(params[:id])
     if user == current_user
@@ -25,6 +38,12 @@ class Admin::UsersController < Admin::BaseController
     user = User.find(params[:id])
     user.update!(admin: true)
     redirect_to admin_users_path, notice: "User promoted to admin"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
 
