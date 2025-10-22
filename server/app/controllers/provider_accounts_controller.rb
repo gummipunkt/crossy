f477eade44daeb2af2c1b981058255b4d3942be1
gummipunkt <patrick@gummipunkt.eu>
2025-10-22 15:29:19 +0200

    feat(server): add production and development docker-compose files and update .gitignore

--- server/app/controllers/provider_accounts_controller.rb
+++ server/app/controllers/provider_accounts_controller.rb
@@ -11,14 +11,57 @@ class ProviderAccountsController < ApplicationController
 
     case provider
     when "mastodon"
+      handle = params.require(:handle)
+      instance = params.require(:instance).to_s.strip
+      token = params.require(:access_token).to_s.strip
+
+      # Normalisiere Instanz ähnlich zum Model-Callback
+      unless instance.start_with?("http://", "https://")
+        instance = "https://#{instance}"
+      end
+      instance = instance.sub(%r{/+$}, "")
+
+      # Validiere Token gegen Instanz
+      conn = Faraday.new(url: instance) { |f| f.adapter Faraday.default_adapter }
+      verify = conn.get("/api/v1/accounts/verify_credentials") do |r|
+        r.headers["Authorization"] = "Bearer #{token}"
+        r.headers["Accept"] = "application/json"
+      end
+      unless verify.success?
+        raise "Mastodon-Token ungültig (#{verify.status}): #{verify.body}"
+      end
+
+      # Versuche Scopes zu ermitteln und auf write:statuses zu prüfen
+      scopes_string = nil
+      begin
+        info = conn.get("/oauth/token/info") do |r|
+          r.headers["Authorization"] = "Bearer #{token}"
+          r.headers["Accept"] = "application/json"
+        end
+        if info.success?
+          body = (JSON.parse(info.body) rescue {})
+          raw_scopes = body["scopes"]
+          scopes = raw_scopes.is_a?(Array) ? raw_scopes : raw_scopes.to_s.split(/\s+/)
+          scopes_string = scopes.join(" ")
+          unless scopes.include?("write:statuses")
+            raise "Mastodon-Token: erforderlicher Scope fehlt: write:statuses"
+          end
+        end
+      rescue => _e
+        # Einige Server haben kein /oauth/token/info – in dem Fall fahren wir fort,
+        # aber speichern keine Scopes.
+        scopes_string ||= nil
+      end
+
       pa = ProviderAccount.create!(
         provider: "mastodon",
-        handle: params.require(:handle),
-        instance: params.require(:instance),
-        access_token: params.require(:access_token),
+        handle: handle,
+        instance: instance,
+        access_token: token,
+        scopes: scopes_string,
         user_id: current_user.id
       )
-      redirect_to provider_accounts_path, notice: "Mastodon connected: #{pa.handle}"
+      redirect_to provider_accounts_path, notice: "Mastodon verbunden: #{pa.handle}"
 
     when "bluesky"
       pa = ProviderAccount.find_or_create_by!(
