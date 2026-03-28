f4261ab95e6d63476e9e61a8c5a1ba3559255f3c
gummipunkt <patrick@gummipunkt.eu>
2026-03-28 18:18:36 +0100

    Fix SsrfSafeUrlValidator load error in production eager load
    module_function is only valid on Module; the autoloaded constant is a
    class. Replace with self.validate! and private helpers in class << self.

--- server/app/services/ssrf_safe_url_validator.rb
+++ server/app/services/ssrf_safe_url_validator.rb
@@ -13,9 +13,7 @@ class SsrfSafeUrlValidator
   DISALLOWED_HOSTNAMES = %w[localhost].freeze
   DNS_TIMEOUT_SEC = 5
 
-  module_function
-
-  def validate!(url_string, allow_http: !Rails.env.production?)
+  def self.validate!(url_string, allow_http: !Rails.env.production?)
     uri = URI.parse(url_string.to_s.strip)
     raise Error, "URL scheme not allowed" unless allowed_scheme?(uri.scheme, allow_http: allow_http)
     raise Error, "host required" if uri.host.blank?
@@ -33,46 +31,45 @@ class SsrfSafeUrlValidator
     uri
   end
 
-  def allowed_scheme?(scheme, allow_http:)
-    return false if scheme.blank?
+  class << self
+    private
 
-    case scheme.downcase
-    when "https" then true
-    when "http" then allow_http
-    else false
+    def allowed_scheme?(scheme, allow_http:)
+      return false if scheme.blank?
+
+      case scheme.downcase
+      when "https" then true
+      when "http" then allow_http
+      else false
+      end
+    end
+
+    def ip_literal(host)
+      IPAddr.new(host)
+    rescue IPAddr::InvalidAddressError
+      nil
+    end
+
+    def raise_disallowed_ip!(ip)
+      raise Error, "IP address not allowed" if disallowed_ip?(ip)
+    end
+
+    def disallowed_ip?(ip)
+      ip.loopback? || ip.private? || ip.link_local?
+    end
+
+    def resolve_and_check_ips!(host)
+      addresses = Timeout.timeout(DNS_TIMEOUT_SEC) { Resolv.getaddresses(host) }
+      raise Error, "host could not be resolved" if addresses.empty?
+
+      addresses.each do |addr|
+        ip = IPAddr.new(addr)
+        raise_disallowed_ip!(ip)
+      end
+    rescue Timeout::Error
+      raise Error, "DNS resolution timed out"
+    rescue IPAddr::InvalidAddressError
+      raise Error, "invalid resolved address"
     end
   end
-  private_class_method :allowed_scheme?
-
-  def ip_literal(host)
-    IPAddr.new(host)
-  rescue IPAddr::InvalidAddressError
-    nil
-  end
-  private_class_method :ip_literal
-
-  def raise_disallowed_ip!(ip)
-    raise Error, "IP address not allowed" if disallowed_ip?(ip)
-  end
-  private_class_method :raise_disallowed_ip!
-
-  def disallowed_ip?(ip)
-    ip.loopback? || ip.private? || ip.link_local?
-  end
-  private_class_method :disallowed_ip?
-
-  def resolve_and_check_ips!(host)
-    addresses = Timeout.timeout(DNS_TIMEOUT_SEC) { Resolv.getaddresses(host) }
-    raise Error, "host could not be resolved" if addresses.empty?
-
-    addresses.each do |addr|
-      ip = IPAddr.new(addr)
-      raise_disallowed_ip!(ip)
-    end
-  rescue Timeout::Error
-    raise Error, "DNS resolution timed out"
-  rescue IPAddr::InvalidAddressError
-    raise Error, "invalid resolved address"
-  end
-  private_class_method :resolve_and_check_ips!
 end
